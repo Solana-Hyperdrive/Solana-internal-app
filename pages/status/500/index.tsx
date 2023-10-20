17a45de1cd3cf4f0cd99d1f5d723714b060134f9
Pranav Rajveer <rajveer23pr@gmail.com>
2023-10-21 00:37:09 +0530

    text-fix

--- pages/status/500/index.tsx
+++ pages/status/500/index.tsx
@@ -117,7 +117,7 @@ function Status500() {
               <Container maxWidth="sm">
                 <Box textAlign="center">
                   <TypographyPrimary variant="h1" sx={{ my: 2 }}>
-                    Tokyo Free Black Next.js Typescript Admin Dashboard
+                  FlitchPay - All In One UPI For Web 3.0
                   </TypographyPrimary>
                   <TypographySecondary
                     variant="h4"
