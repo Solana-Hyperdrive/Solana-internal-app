7d850a7a9294a7197797721ecccc501cc5fe2cc2
moonlightnexus <68702919+moonlightnexus@users.noreply.github.com>
2023-10-20 21:08:48 +0000

    modified:   src/content/Overview/Hero/index.tsx

--- src/content/Overview/Hero/index.tsx
+++ src/content/Overview/Hero/index.tsx
@@ -105,9 +105,9 @@ function Hero() {
         container
       >
         <Grid item md={10} lg={8} mx="auto">
-          <LabelWrapper color="success">Version 1.0.0</LabelWrapper>
+          <LabelWrapper color="success">Version 0.4.0</LabelWrapper>
           <TypographyH1 sx={{ mb: 2 }} variant="h1">
-            Tokyo Free Black Next.js Typescript Admin Dashboard
+            Flitch-Pay
           </TypographyH1>
           <TypographyH2
             sx={{ lineHeight: 1.5, pb: 4 }}
@@ -115,9 +115,8 @@ function Hero() {
             color="text.secondary"
             fontWeight="normal"
           >
-            High performance React template built with lots of powerful
-            Material-UI components across multiple product niches for fast &
-            perfect apps development processes
+            We use Solana’s & SPL to build a crypto payment interface that is fast, unique & Reliable.
+            You can call us maybe Stripe* of crypto. We enjoy developing this project and will keep improving it.
           </TypographyH2>
           <Button
             component={Link}
@@ -125,67 +124,20 @@ function Hero() {
             size="large"
             variant="contained"
           >
-            Browse Live Preview
+            Go to App
           </Button>
           <Button
             sx={{ ml: 2 }}
             component="a"
             target="_blank"
             rel="noopener"
-            href="https://bloomui.com/product/tokyo-free-black-react-nextjs-material-ui-admin-dashboard"
+            href="https://docs.flitchcoin.com"
             size="large"
             variant="text"
           >
-            Key Features
+            Docs
           </Button>
           <Grid container spacing={3} mt={5}>
-            <Grid item md={4}>
-              <MuiAvatar>
-                <img
-                  src="/static/images/logo/material-ui.svg"
-                  alt="Material-UI"
-                />
-              </MuiAvatar>
-              <Typography variant="h4">
-                <Box sx={{ pb: 2 }}>
-                  <b>Powered by MUI (Material-UI)</b>
-                </Box>
-                <Typography component="span" variant="subtitle2">
-                  A simple and customizable component library to build faster,
-                  beautiful, and accessible React apps.
-                </Typography>
-              </Typography>
-            </Grid>
-            <Grid item md={4}>
-              <NextJsAvatar>
-                <img src="/static/images/logo/next-js.svg" alt="NextJS" />
-              </NextJsAvatar>
-              <Typography variant="h4">
-                <Box sx={{ pb: 2 }}>
-                  <b>Built with Next.js</b>
-                </Box>
-                <Typography component="span" variant="subtitle2">
-                  Next.js gives you the best developer experience with all the
-                  features you need for production.
-                </Typography>
-              </Typography>
-            </Grid>
-            <Grid item md={4}>
-              <TsAvatar>
-                <img
-                  src="/static/images/logo/typescript.svg"
-                  alt="Typescript"
-                />
-              </TsAvatar>
-              <Typography variant="h4">
-                <Box sx={{ pb: 2 }}>
-                  <b>Built with Typescript</b>
-                </Box>
-                <Typography component="span" variant="subtitle2">
-                  Tokyo Free Black features a modern technology stack and is built with React + Typescript.
-                </Typography>
-              </Typography>
-            </Grid>
           </Grid>
         </Grid>
       </Grid>
