9d6a2fc43e409121186f32f307c371b8c34d7e0d
Suryansh Anand <spirit@anandsur.com>
2023-11-24 20:45:14 +0530

    feat: loading shimmer on all products route

--- src/content/Management/Product/AllProductsTable.tsx
+++ src/content/Management/Product/AllProductsTable.tsx
@@ -4,6 +4,7 @@ import {
   CardHeader,
   Divider,
   IconButton,
+  Skeleton,
   Table,
   TableBody,
   TableCell,
@@ -30,12 +31,14 @@ const applyPagination = (allProducts, page: number, limit: number) => {
 const AllProductsTable = () => {
   const queryClient = useQueryClient();
 
-  const { data: products, isLoading } = useQuery(['products'], async () =>
-    axios.get('https://ledger.flitchcoin.com/all/product', {
-      headers: {
-        Authorization: `Bearer ${localStorage.getItem('accessToken')}`
-      }
-    })
+  const { data: products, isLoading: isProductsLoading } = useQuery(
+    ['products'],
+    async () =>
+      axios.get('https://ledger.flitchcoin.com/all/product', {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem('accessToken')}`
+        }
+      })
   );
 
   const [page, setPage] = useState<number>(0);
@@ -63,126 +66,130 @@ const AllProductsTable = () => {
     queryClient.invalidateQueries({ queryKey: ['products'] });
   }
 
-  if (isLoading) {
-    return <p>Loading...</p>;
-  }
-
   return (
     <Card>
       <CardHeader title="Products" />
 
       <Divider />
 
-      <TableContainer>
-        <Table>
-          <TableHead>
-            <TableRow>
-              <TableCell>Name</TableCell>
-              <TableCell>Product ID</TableCell>
-              <TableCell>Description</TableCell>
-              <TableCell align="right">Price (USD)</TableCell>
-              <TableCell align="right">Checkout Page</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {paginatedProducts?.map((product) => (
-              <TableRow hover key={product.uuid}>
-                <TableCell>
-                  <Typography
-                    variant="body1"
-                    fontWeight="bold"
-                    color="text.primary"
-                    gutterBottom
-                    noWrap
-                  >
-                    {product.name}
-                  </Typography>
-                </TableCell>
+      {isProductsLoading ? (
+        <Skeleton width="100vw" height="50vh" />
+      ) : (
+        <>
+          <TableContainer>
+            <Table>
+              <TableHead>
+                <TableRow>
+                  <TableCell>Name</TableCell>
+                  <TableCell>Product ID</TableCell>
+                  <TableCell>Description</TableCell>
+                  <TableCell align="right">Price (USD)</TableCell>
+                  <TableCell align="right">Checkout Page</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {paginatedProducts?.map((product) => (
+                  <TableRow hover key={product.uuid}>
+                    <TableCell>
+                      <Typography
+                        variant="body1"
+                        fontWeight="bold"
+                        color="text.primary"
+                        gutterBottom
+                        noWrap
+                      >
+                        {product.name}
+                      </Typography>
+                    </TableCell>
 
-                <TableCell>
-                  <Typography
-                    variant="body1"
-                    fontWeight="bold"
-                    color="text.primary"
-                    gutterBottom
-                    noWrap
-                  >
-                    {product.product_id}
-                  </Typography>
-                </TableCell>
+                    <TableCell>
+                      <Typography
+                        variant="body1"
+                        fontWeight="bold"
+                        color="text.primary"
+                        gutterBottom
+                        noWrap
+                      >
+                        {product.product_id}
+                      </Typography>
+                    </TableCell>
 
-                <TableCell>
-                  <Typography
-                    variant="body1"
-                    fontWeight="bold"
-                    color="text.primary"
-                    gutterBottom
-                  >
-                    {product.description}
-                  </Typography>
-                </TableCell>
+                    <TableCell>
+                      <Typography
+                        variant="body1"
+                        fontWeight="bold"
+                        color="text.primary"
+                        gutterBottom
+                      >
+                        {product.description}
+                      </Typography>
+                    </TableCell>
 
-                <TableCell align="right">
-                  <Typography
-                    variant="body1"
-                    fontWeight="bold"
-                    color="text.primary"
-                    gutterBottom
-                    noWrap
-                  >
-                    {product.price}
-                  </Typography>
-                </TableCell>
+                    <TableCell align="right">
+                      <Typography
+                        variant="body1"
+                        fontWeight="bold"
+                        color="text.primary"
+                        gutterBottom
+                        noWrap
+                      >
+                        {product.price}
+                      </Typography>
+                    </TableCell>
 
-                <TableCell align="right">
-                  <Typography
-                    variant="body1"
-                    fontWeight="bold"
-                    color="text.primary"
-                    gutterBottom
-                    noWrap
-                  >
-                    <Link
-                      href={`https://ledger.flitchcoin.com/checkout/page?qp=${product.link}`}
-                    >
-                      Go
-                    </Link>
-                  </Typography>
-                </TableCell>
+                    <TableCell align="right">
+                      <Typography
+                        variant="body1"
+                        fontWeight="bold"
+                        color="text.primary"
+                        gutterBottom
+                        noWrap
+                      >
+                        <Link
+                          href={`https://ledger.flitchcoin.com/checkout/page?qp=${product.link}`}
+                        >
+                          Go
+                        </Link>
+                      </Typography>
+                    </TableCell>
 
-                <TableCell align="right">
-                  <Tooltip title="Delete Product" arrow>
-                    <IconButton
-                      sx={{
-                        '&:hover': { background: theme.colors.error.lighter },
-                        color: theme.palette.error.main
-                      }}
-                      color="inherit"
-                      size="small"
-                      onClick={() => handleDeleteProduct(product.uuid)}
-                    >
-                      <DeleteTwoToneIcon fontSize="small" />
-                    </IconButton>
-                  </Tooltip>
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
+                    <TableCell align="right">
+                      <Tooltip title="Delete Product" arrow>
+                        <IconButton
+                          sx={{
+                            '&:hover': {
+                              background: theme.colors.error.lighter
+                            },
+                            color: theme.palette.error.main
+                          }}
+                          color="inherit"
+                          size="small"
+                          onClick={() => handleDeleteProduct(product.uuid)}
+                        >
+                          <DeleteTwoToneIcon fontSize="small" />
+                        </IconButton>
+                      </Tooltip>
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
 
-      {/* Pagination Filter */}
-      <Box p={2}>
-        <TablePagination
-          component="div"
-          count={products?.data?.length}
-          onPageChange={handlePageChange}
-          onRowsPerPageChange={handleLimitChange}
-          page={page}
-          rowsPerPage={limit}
-          rowsPerPageOptions={[5, 10, 25, 30]}
-        />
-      </Box>
+          {/* Pagination Filter */}
+          <Box p={2}>
+            <TablePagination
+              component="div"
+              count={products?.data?.length}
+              onPageChange={handlePageChange}
+              onRowsPerPageChange={handleLimitChange}
+              page={page}
+              rowsPerPage={limit}
+              rowsPerPageOptions={[5, 10, 25, 30]}
+            />
+          </Box>
+        </>
+      )}
     </Card>
   );
 };
